84be4c99a67a0c8e9b63c8e0dc7e026f31bccd63
NotANick <ilya.vailov@gmail.com>
2024-05-14 13:45:45 +0300

    Tests

---
+++ ForTests/TestAccessModifiersScanning.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTests;
+
+internal class TestAccessModifiersScanning
+{
+    private int a()
+    {
+        return 0;
+    }
+
+    private protected int b()
+    {
+        return 0;
+    }
+
+    internal int c()
+    {
+        return 0;
+    }
+
+    protected int d()
+    {
+        return 0;
+    }
+
+    protected internal int e()
+    {
+        return 0;
+    }
+
+    public int f()
+    {
+        return 0;
+    }
+}
